c88b0896880512e6846853ac608f7413c3a22598
李俊峰 <lijunfeng@u51.com>
2019-01-16 20:20:10 +0800

    事件处理。

---
+++ 事件处理/click.js
@@ -0,0 +1,30 @@
+var example1 = new Vue({
+    el: '#example-1',
+    data: {
+        counter: 0
+    }
+})
+
+var example2 = new Vue({
+    el: '#example-2',
+    data: {
+        name: 'Vue.js'
+    },
+    methods: {
+        greet: function (event) {
+            alert('Hello ' + this.name + '!')
+            if (event) {
+                alert(event.target.tagName)
+            }
+        }
+    }
+})
+
+var example3 = new Vue({
+    el: '#example-3',
+    methods: {
+        say: function (message) {
+            alert(message)
+        }
+    }
+})
