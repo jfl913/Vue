016020daaccebc7f81c7cdaa72547d47a194170e
李俊峰 <lijunfeng@u51.com>
2019-01-18 10:46:48 +0800

    表单输入绑定。

---
+++ 表单输入绑定/form.js
@@ -0,0 +1,21 @@
+var app1 = new Vue({
+    el: '#app-1',
+    data: {
+        message: ''
+    }
+})
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message2: ''
+    }
+})
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        checked: false,
+        checkedNames: []
+    }
+})
