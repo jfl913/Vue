f7384f1bcbd09e6cb61ed06acd47291831d604a5
李俊峰 <lijunfeng@u51.com>
2019-01-18 19:23:19 +0800

    组件基础。

---
+++ 组件基础/component.js
@@ -0,0 +1,21 @@
+Vue.component('button-counter', {
+    data: function () {
+        return {
+            count: 0
+        }
+    },
+    template: '<button v-on:click="count++">You clicked me {{ count }} times</button>>'
+})
+
+var componentsDemo = new Vue({
+    el: '#components-demo'
+})
+
+Vue.component('blog-post', {
+    props: ['title'],
+    template: '<h3>{{ title }}</h3>'
+})
+
+var blogPostDemo = new Vue({
+    el: '#blog-post-demo'
+})
